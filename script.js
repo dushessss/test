fbff85565fcc7eeff75aac513590785c4a4187ee
Andrew Cheremisin <85918059+dushessss@users.noreply.github.com>
2024-02-23 18:23:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,20 +13,9 @@ send.onclick = function () {
 
     })
         .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log('error', error));
-};
-get.onclick = function (){
-    const formData = new FormData();
-    fetch("https://api.imgur.com/3/image/",{
-        method: 'POST',
-        headers:{
-            "Authorization" : "Client-ID f175ff8c2246196"
-        },
-        body: formData,
-        redirect: 'follow'
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => data.json)
+        .then(data => {
+            url.innerText = data.data.link
+        })
         .catch(error => console.log('error', error));
 };
